7d0860b58361cf72dcce14bdf13dcda80e06d1a8
PepeNeviz <ziven642@gmail.com>
2025-03-10 11:44:12 +0700

    celcius.py

--- celcius.py
+++ celcius.py
@@ -1,8 +1,3 @@
-# Meminta pengguna memasukkan suhu dalam Celsius
 celsius = float(input("Masukkan suhu dalam derajat Celsius: "))
-
-# Mengonversi ke Fahrenheit
 fahrenheit = (celsius * 9/5) + 32
-
-# Menampilkan hasil
 print(f"{celsius}°C sama dengan {fahrenheit:.2f}°F")
